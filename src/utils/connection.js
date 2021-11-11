dce5faa97ca43e35f88951c0b44d2ccc2be8173f
Teepan <paartheepan94@gmail.com>
2021-11-11 16:13:07 +0100

    chore: use of variable for mongo host

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const connectDb = () => {
   mongoose
     .connect(
-      `mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}@mongodb:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}?authSource=admin`,
+      `mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}@${process.env.MONGO_HOST ?? 'mongodb'}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}?authSource=admin`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
