41d88c494ab6973268cb0935f61b239851b0471e
Teepan <paartheepan94@gmail.com>
2021-11-11 16:58:24 +0100

    chore: fix app running port for production

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from './app.js'
 
-const PORT = process.env.API_PORT || 5000
+const PORT = process.env.NODE_ENV === 'production' ? process.env.PORT : process.env.API_PORT || 5000
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}: http://localhost:${PORT}`)
