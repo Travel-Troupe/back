3e4d0dd0b40225b0a942a224c945d12263ff8738
Teepan <paartheepan94@gmail.com>
2021-11-13 21:16:33 +0100

    style: lint

--- src/config/strategy.js
+++ src/config/strategy.js
@@ -9,7 +9,7 @@ const strategy = new Auth0Strategy(
     domain: process.env.AUTH0_DOMAIN,
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL: process.env.AUTH0_CALLBACK_URL || isProduction ? PROD_URL : /`http://localhost:${PORT}/callback`
+    callbackURL: process.env.AUTH0_CALLBACK_URL || isProduction ? PROD_URL : `http://localhost:${PORT}/callback`
   },
   function (accessToken, refreshToken, extraParams, profile, done) {
     return done(null, profile)
