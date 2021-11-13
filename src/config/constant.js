85ac4aaaf99fb29c36ad4d7d41bab9de6dc247ee
Teepan <paartheepan94@gmail.com>
2021-11-13 22:29:58 +0100

    fix: callback uri

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,2 +1,2 @@
 export const PORT = process.env.NODE_ENV === 'production' ? process.env.PORT : process.env.API_PORT || 5000
-export const PROD_URL = 'https://travel-troupe-api.herokuapp.com/'
+export const PROD_URL = 'https://travel-troupe-api.herokuapp.com'
